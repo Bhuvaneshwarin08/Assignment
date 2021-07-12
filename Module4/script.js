61f7cbcdaf9eeeca0870a3746d23f24ae467af67
Bhuvaneshwarin08 <86528904+Bhuvaneshwarin08@users.noreply.github.com>
2021-07-12 22:26:21 +0530

    Update script.js

--- Module4/script.js
+++ Module4/script.js
@@ -1,9 +1,9 @@
 var names=["Yaakov","John","Jen","jason","paul","frank","larry","paula","laura","jim"];
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-            console.log("Goodbye "+ names[i])
+            console.log("Goodbye "+ names[i]);
 	}
 	else{
-	    console.log("Hello "+ names[i])
+	    console.log("Hello "+ names[i]);
 	}
 }
